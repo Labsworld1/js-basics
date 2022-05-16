46fda019644fe1b8146faecb806394604087d9e1
Azeez Lameed <azeezlameed@gmail.com>
2022-05-16 22:16:11 +0100

    jsStudy

---
+++ index.js
@@ -0,0 +1,20 @@
+let person = {
+    firstName: ' ',
+    lastName: ' ',
+    age: 30,
+    occupation: '',
+    gender: ['male', 'female'],
+    isMarried: false,
+    canRead: false
+};
+
+function greet (firstName) {
+    console.log('Hello ' + firstName)
+}
+
+
+function root(number) {
+    return number * number;
+}
+
+console.log(root(20));
